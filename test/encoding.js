db21e3c5d45de818e017141818f9dc6dea0b48d9
suisho <suisho000000@gmail.com>
2013-03-02 00:40:44 +0900

    add test

--- test/encoding.js
+++ test/encoding.js
@@ -1,9 +1,27 @@
 var fs = require("fs")
-var encoder = require("../index.js")
+var request = require("../index.js")
+var assert = require("assert")
+var connect = require("connect")
+var app = connect()
+app.use(connect.static(__dirname+"/fixtures")).listen(9765)
+
 describe('what?', function () {
-  it('should do what?', function (done) {
-    var request = encoder.request( "http://www.itmedia.co.jp/", function(error, response, body){
-      console.log(body);
+  var utf8Result;
+  before(function (done) {
+    request( "http://localhost:9765/utf8.html", function(error, response, body){
+      utf8Result = body;
+      done();
+    })
+  });
+  it('Sjis page request', function (done) {
+    request( "http://localhost:9765/sjis.html", function(error, response, body){
+      assert.equal(body, utf8Result);
+      done();
+    })
+  });
+  it('Eucjp page request', function (done) {
+    request( "http://localhost:9765/Eucjp.html", function(error, response, body){
+      assert.equal(body, utf8Result);
       done();
     })
   });
